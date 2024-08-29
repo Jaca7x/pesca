615c0b047d1d79d5c36125b1fba782c084aa3bb1
Jaca1x <117647243+Jaca7x@users.noreply.github.com>
2024-08-29 20:11:22 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,168 @@
+export const perguntas = [
+    {
+        enunciado: "Assim que você saiu para pescar com seus amigos teve que mostrar que realmente era um mestre da pesca os levando para o melhor ponto de pesca, mas utilizando a IA",
+        alternativas: [
+            {
+                texto: "Pantanal!",
+                afirmacao: [
+                    "Muitos acharam que ele era louco mas ele estava correto!",
+                    "Acaharam o Pantanal uma exelente ideia!"
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Rio Trombetas!",
+                afirmacao: [
+                    "Um local de dificil acesso mas com grande variedade de peixes.",
+                    "Acharam loucura pois o Rio fica praticamente isolado em meio à floresta. Localiza-se a 400km de Manaus numa região de difícil acesso."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Após a bela indicação que você fez sobre o local seus amigos estavam que duvidas em que boias utilizarem para um melhor rendimento na pesca.  Assim pedindo para que você desse sua opinião, então você com seu conhecimento avançado de pesca logo pergunto para IA qual seria a melhor escolha.",
+        alternativas: [
+            {
+                texto: "Utilizar uma boia de equipamento mais pesado",
+                afirmacao: [
+                    "A boia pesada e uma otima escolha para ter uma arremesso mais longo com certos equipamentos. Todos concordaram.",
+                    "As boias pesadas são muito uteis na hora dos gigantes dos lagos. Acharam uma bela dica.",
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Boia de Torpedo da temporada.",
+                afirmacao: [
+                    "A boia torpedo e muito boa para arremessos. Acharam uma otima dica.",
+                    "Boia torpedo muito boa para peixes gigantes. Gostaram da informação",
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Um dos colegas de você estava em dúvida de qual de suas varas utilizar para pescaria de Tilápias, então decide perguntar para você qual deveria ser sua escolha, como você tem um grande conhecimento de pesca ele logo decide pergunta para sua grande amiga IA qual seria a melhor escolha.",
+        alternativas: [
+            {
+                texto: "Varinha Joker.",
+                afirmacao: [
+                    "Uma varinha muito boa para pesca da Tilápia, adorou a dica por ser uma varinha que dava para utilizar no local que estavam",
+                    "Não gostou muito da dica pois a varinha era muito sensivel e acabou quebrando na hora da fisgada..",
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Vara HXT immortal.",
+                afirmacao: [
+                    "Gostou da dica, e achou uma otima vara para pesca.",
+                    "Não gostou muito da vara por conta do seu tamanho.",
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Após a escolha das varas e outros equipamentos estava na hora de escolher quantos metros de linha era o ideal para esse dia, então novamente perguntaram para você qual era o tamanho ideal e novamente você pediu ajuda para sua amiga IA.",
+        alternativas: [
+            {
+                texto: "120 a 170 metros de linha.",
+                afirmacao: [
+                    "Acharam o tamanho ideal para pesca em água doce.",
+                    "Tamanho perfeito para onde estavam!"
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "150 a 350 metros de linha.",
+                afirmacao: [
+                    "Acharam o tamanho da linha um pouco exagerado para onde estavam.",
+                    "Muitos acharam que você estava louco por conta dessa informação"
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Bom além dos equipamentos de pesca e claro que não poderia faltar uma bela roupas de pesca qual protege do sol, então novamente pediram a informção para você. E você recomendou!",
+        alternativas: [
+            {
+                texto: "Quisty.",
+                afirmacao: [
+                    "Por se tratar de uma marca de pesca espotiva acharam uma otima ideia.",
+                    "Gostaram muito das roupas da marca!"
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Nike",
+                afirmacao: [
+                    "Pensaram que você estava louco.",
+                    "Isso não é uma marca de esportes?"
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "E claro que após todo esse preparo com o equipamento e obvio que não pode faltar a embarcação que os pescadores irão utilizar e com a ajuda de você e com as informações da IA as alternativas foram!.",
+        alternativas: [
+            {
+                texto: "Semichata",
+                afirmacao: [
+                    "Semichata e uma pequena embarcação muito utilizada na pesca.",
+                    "Uma embarcação perfeita para a pesca."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Escuna",
+                afirmacao: [
+                    "E um bom barco mas não e o mlehor para pesca.",
+                    "Barco muito grande para rios e lagos estreitos."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "Claro que uma boa pescaria não pode faltar aquela musiquinha para acompanha os pescadores!! Com a ajuda da IA você acha a melhor musica para pesca.",
+        alternativas: [
+            {
+                texto: "Um modão dos bons",
+                afirmacao: [
+                    "Pela maioria dos pescadores seram velhos eles gostaram.",
+                    "As melhores musicas para a pesca"
+                ],
+                proxima: 8,
+            },
+            {
+                texto: "Pablo Vitar",
+                afirmacao: [
+                    "Os pescadores acabaram te jogando para fora do barco",
+                    "você percebeu que todos estavam olhando de uma forma diferente para ele."
+                ],
+                proxima: 8,
+            },
+        ]
+    },
+    {
+        enunciado: "No final da pescaria e a hora que todos decidem quais peixes irão levar para suas casa! mas estava uma briga danada pelos maiores peixes e com isso você decide utilizar o conhecimento da IA novamente para decidirem de forma correta Mas você não achou um meio correto para fazer divissão então decide pensar sozinho em uma solução e...",
+        alternativas: [
+            {
+                texto: "você pega os maiores peixes e sai correndo!",
+                afirmacao: [
+                    "Seus colegas de pesca ficaram furiosos com você a ponto de te tirarem do grupo de pesca.",
+                    "Os pescadores correram atrás de você loucamente."
+                ],
+            },
+            {
+                texto: "você faz uma divissão justa",
+                afirmacao: [
+                    "Alguns colegas de pesca não gostaram muito da ideia mas aceitaram",
+                    "Todos aceitaram sua ideia.",
+                ],
+            },
+        ]
+    }
+];
